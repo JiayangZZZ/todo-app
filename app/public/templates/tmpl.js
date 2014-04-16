9e613971881a72e3c1c8a539d9cdd52621188972
Jiayang Zhang <jiayangzhang1104@gmail.com>
2014-04-16 19:22:34 +0800

    removed seperated body

--- app/public/templates/tmpl.js
+++ app/public/templates/tmpl.js
@@ -8,12 +8,6 @@ var out='<section><p>I love this</p></section>';return out;
   tmpl['bodyTodo']=function anonymous(it) {
 var out='<section>'+(it.todo)+'</section>';return out;
 };
-  tmpl['container']=function anonymous(it) {
-var out='<section class="container"><p>asdfjadfg</p><ul><li>item</li><li>item</li><li>item</li><li>item</li><li>item</li><li>item</li><li>asldkjghajs</li><li>asldkjghajs</li><li>asldkjghajs</li><li>item</li><li>item</li><li>item</li><li>item</li><li>item</li></ul></section>';return out;
-};
-  tmpl['description']=function anonymous(it) {
-var out='<section><p>123445</p></section>';return out;
-};
   tmpl['footer']=function anonymous(it) {
 var out='<section class="footer"></section>';return out;
 };
@@ -22,8 +16,5 @@ var out='<header class="landing-header"><section class="title"><p>header title</
 };
   tmpl['html']=function anonymous(it) {
 var out='<!DOCTYPE html><html><head><title>TODO APP</title><link href="/public/styles/build/styles.css" rel="stylesheet" type="text/css"></head><body>'+(it.header)+(it.body)+(it.footer)+'</body></html>';return out;
-};
-  tmpl['list']=function anonymous(it) {
-var out='';return out;
 };
 return tmpl;});
