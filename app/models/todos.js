4ff813d46850c567263862f17a533e7982adb64b
Jiayang Zhang <jiayangzhang1104@gmail.com>
2014-05-29 09:29:48 +0800

    passing in todo's title

--- app/models/todos.js
+++ app/models/todos.js
@@ -1,10 +1,38 @@
 
-var todos = [{
-  title : 'first todo'
-},{
-  title : 'second todo'
-},{
-  title : 'a;lsfjgla todo'
-}];
+// var todos = [{
+//   title : 'first todo'
+// },{
+//   title : 'second todo'
+// },{
+//   title : 'a;lsfjgla todo'
+// }];
 
-module.exports = todos;
+var request = require('superagent')
+  , util = require('util')
+  , Model = require('./model')
+  , Session = require('./session');
+
+function Todos() {
+}
+
+util.inherits(Todos, Model);
+
+Todos.prototype.get = function() {
+  request
+    .get(this.read().origin + '/todos/?userId=1&accessToken=' + this.read().accessToken )
+    .send({
+      username : 'jiayang',
+      password : 'password',
+      clientId : this.read().clientId,
+      clientSecret : this.read().clientSecret
+    })
+    .end(function(data) {
+      console.log(data.body);
+    });
+};
+
+// var todos = new Todos();
+// todos.get();
+
+
+// module.exports = todos;
--- app/public/templates/tmpl.js
+++ app/public/templates/tmpl.js
@@ -9,13 +9,13 @@ var out='<div class="body-index"><div class="container"><div class="wrapper-inpu
 var out='<div class="body-index"><div class="container"><ul class="list">';var arr1=it.listItems;if(arr1){var listItem,index=-1,l1=arr1.length-1;while(index<l1){listItem=arr1[index+=1];out+='<li class="list-item">'+(listItem.title)+'</li>';} } out+='</ul><a href="http://zhangjiayang.dev.p1staff.com:3200/create" class="create-button"><p class="create-button-text">CREATE</p></a></div></div>';return out;
 };
   tmpl['bodyTodo']=function anonymous(it) {
-var out='<div class="body-index"><div class="container"><div class="todo-description"><p>'+(it.todo)+'</p></div><div><a href="http://zhangjiayang.dev.p1staff.com:3200" class="create-button"><p class="create-button-text">BACK</p></a></div><div><a href="http://zhangjiayang.dev.p1staff.com:3200" class="delete-button"><p class="delete-button-text">DELETE</p></a></div></div></div>';return out;
+var out='<div class="body-index"><div class="container"><div class="todo-description"><p>'+(it.description)+'</p></div><div><a href="http://zhangjiayang.dev.p1staff.com:3200" class="create-button"><p class="create-button-text">BACK</p></a></div><div><a href="http://zhangjiayang.dev.p1staff.com:3200" class="delete-button"><p class="delete-button-text">DELETE</p></a></div></div></div>';return out;
 };
   tmpl['footer']=function anonymous(it) {
 var out='<section class="footer"></section>';return out;
 };
   tmpl['header']=function anonymous(it) {
-var out='<header class="landing-header"><section class="landing-header-container"><h1 class="title">YOUR TODOS</h1></section></header>';return out;
+var out='<header class="landing-header"><section class="landing-header-container"><h1 class="title">'+(it.title)+'</h1></section></header>';return out;
 };
   tmpl['html']=function anonymous(it) {
 var out='<!DOCTYPE html><html><head><title>TODO APP</title><link href="/public/styles/build/styles.css" rel="stylesheet" type="text/css"></head><body>'+(it.header)+(it.body)+'<!--     '+(it.footer)+' --></body></html>';return out;
